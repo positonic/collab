cd3f920ba78cee861a51b3ecbf4d9cd54bcee1e7
James Farrell <info@jamespfarrell.com>
2020-05-15 16:59:48 +0200

    change og image

--- src/components/GroupRoll.js
+++ src/components/GroupRoll.js
@@ -47,7 +47,7 @@ class GroupRoll extends React.Component {
                   <br />
                   <br />
                   <Link className='button' to={post.fields.slug}>
-                    Keep Reading →
+                    View discussion →
                   </Link>
                 </p>
               </article>
